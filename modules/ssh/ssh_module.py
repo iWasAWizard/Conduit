32103701d45ea0c6d9e47bb7ef8660926e7913a6
Joey V <infinity1423@gmail.com>
2024-05-29 09:32:04 -0400

    added pieces to ssh_module

--- modules/ssh/ssh_module.py
+++ modules/ssh/ssh_module.py
@@ -1,3 +1,4 @@
+import pexpect
 import paramiko
 
 class ConduitSSHModule:
@@ -13,7 +14,7 @@ class ConduitSSHModule:
         self.log = open(self.logfile, 'a')
         self.client = paramiko.SSHClient()
         self.client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        self.client.connect(self.hostname, port=self.port, username=self.username, key_filename=self.key_filename)
+        self.client.connect(self.hostname, port=self.port, username=self.username, password=self.password, key_filename=self.key_filename)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -22,4 +23,54 @@ class ConduitSSHModule:
 
     def execute_command(self, command):
         stdin, stdout, stderr = self.client.exec_command(command)
-        return stdout.read().decode(), stderr.read().decode()
+        stdout_data = stdout.read().decode()
+        stderr_data = stderr.read().decode()
+        self.log.write(f"COMMAND: {command}\nOUTPUT: {stdout_data}\nERROR: {stderr_data}\n")
+        return stdout_data, stderr_data
+
+    def interactive_session(self, command, timeout=30):
+        child = pexpect.spawn(f'ssh {self.username}@{self.hostname} -p {self.port}', encoding='utf-8')
+        child.logfile = self.log
+
+        if self.password:
+            child.expect('password:')
+            child.sendline(self.password)
+        
+        child.expect('$')
+        child.sendline(command)
+
+        try:
+            child.expect(pexpect.EOF, timeout=timeout)
+            output = child.before
+        except pexpect.TIMEOUT:
+            child.terminate()
+            output = child.before
+            error = 'Command timed out'
+        
+        output = self.remove_ansi_escape_sequences(output)
+        return output, error if 'error' in locals() else ''
+
+    @staticmethod
+    def remove_ansi_escape_sequences(text):
+        ansi_escape_pattern = re.compile(r'''
+            \x1B  # ESC
+            (?:   # 7-bit C1 Fe (except CSI)
+                [@-Z\\-_]
+            |     # or [ for CSI, followed by a control sequence
+                \[
+                [0-?]*  # Parameter bytes
+                [ -/]*  # Intermediate bytes
+                [@-~]   # Final byte
+            )
+        ''', re.VERBOSE)
+        return ansi_escape_pattern.sub('', text)
+
+# Example usage:
+# with ConduitSSHModule(hostname='example.com', username='user', password='password') as ssh:
+#     output, error = ssh.execute_command('ls -l')
+#     print('Output:', output)
+#     print('Error:', error)
+#     interactive_output, interactive_error = ssh.interactive_session('ls -l')
+#     print('Interactive Output:', interactive_output)
+#     print('Interactive Error:', interactive_error)
+
